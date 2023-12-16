8cb6c76e08b407802f7053740b64a09ec32b7244
Songling Wang <wangsonglingchina@163.com>
2023-12-15 22:13:26 -0700

    Update BuyNow.jsx

--- components/BuyNow.jsx
+++ components/BuyNow.jsx
@@ -19,7 +19,7 @@ function BuyNow({productPriceId}) {
 	type="submit"
 	value="submit"
 	className="w-2/5 text-xl font-bold rounded-md bg-white/5 hover:bg-white/20
-	py-2.5 text-white ring-4 ring-white/5 ring-offset-2
+	py-2.5 text-black ring-4 ring-white/5 ring-offset-2
 	ring-offset-white/[0.09]"
 	>
 	Buy Now
